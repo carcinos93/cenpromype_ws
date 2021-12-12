228f6ba3ca520183893fa8837657bd8acd02d0c1
carcinos93 <nelson.rodas1993@gmail.com>
2021-12-11 19:59:01 -0600

    modelo usuariosistema

---
+++ app/Models/TB/UsuariosSistema.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models\TB;
+
+use \App\Models\BaseModel;
+
+class UsuariosSistema extends BaseModel
+{
+    public $incrementing = true;
+    protected $table = 'TB_USUARIOS_SISTEMA';
+    protected $primaryKey = 'ID';
+    protected $fillable = ['USUARIO', 'NOMBRES','APELLIDOS', 'GENERO', 
+                            'ANIO_NAC', 'PAIS_RESIDENCIA','CODIGO_TIPO_INST',
+                            'NOMBRE_INSTITUCION','TAMANIO_EMPRESA','CODIGO_SEC_EMP', 'CODIGO_ACT',
+                            'CORREO_ELECTRONICO','CONTRASENIA','RECIBE_NOTIFICACION',
+                            'ESTADO', 'ACCESO', 'USUARIO_ADICION', 'FECHA_ADICION'];
+    const CREATED_AT = 'FECHA_ADICION';
+    const UPDATED_AT = 'FECHA_MODIFICACION';
+    const KEY = 'ID';
+
+}
